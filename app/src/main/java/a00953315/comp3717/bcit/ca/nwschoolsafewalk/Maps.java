b330efa0ac6bef0919a9cb2f23cf61e3007bda26
Daniel <daniel51_1994@hotmail.com>
2017-02-02 16:35:09 -0800

    location functionality

--- app/src/main/java/a00953315/comp3717/bcit/ca/nwschoolsafewalk/Maps.java
+++ app/src/main/java/a00953315/comp3717/bcit/ca/nwschoolsafewalk/Maps.java
@@ -1,29 +1,97 @@
 package a00953315.comp3717.bcit.ca.nwschoolsafewalk;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Maps extends FragmentActivity implements OnMapReadyCallback {
+public class Maps extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+    public static final int LOCATION_ACCESS_CODE = 1;
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    private static final String DIALOG_ERROR = "dialog_error";
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrentLocation;
+    LocationRequest mLocationRequest;
+    private boolean resolvingError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        checkLocationPermission();
+
+        // Map component to place the map.
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    public void checkLocationPermission(){
+        // Check permissions
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check SDK version
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // Request permission
+                requestPermissions(new String[] {
+                                android.Manifest.permission.ACCESS_FINE_LOCATION },
+                        LOCATION_ACCESS_CODE);
+            }
+        }
+    }
+
+    // Callback for the result from requesting permissions
+    @Override
+    public void onRequestPermissionsResult(int    requestCode,
+                                           String permissions[],
+                                           int[]  grantResults) {
+        switch (requestCode) {
+            case LOCATION_ACCESS_CODE: {
+                // If request is cancelled, the result arrays are empty
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (mGoogleApiClient == null) {
+                        buildGoogleApiClient();
+                    }
+                } else {
+                    Toast.makeText(this, "Allow location access to use the app",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,10 +105,157 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        UiSettings settings = mMap.getUiSettings();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+                settings.setZoomControlsEnabled(true);
+            }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10 * 1000);        // 10 sec
+        mLocationRequest.setFastestInterval(1000);      // 1 sec
+        // Obtain location (high accuracy), requires more power and time
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Check permissions before calling APIs
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+        if (mCurrentLocation != null) {
+            mCurrentLocation.remove();
+        }
+
+        // Obtain current coordinates
+        LatLng coordinates = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions options = new MarkerOptions();
+        options.position(coordinates);
+        options.title("Hmm");
+        // Marker colour
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        mCurrentLocation = mMap.addMarker(options);
+
+        // Move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+        // Zoom level
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        // Stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if(mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if(resolvingError) {
+            return;
+        }
+
+        if(result.hasResolution()) {
+            try {
+                resolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch(final IntentSender.SendIntentException e) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            showErrorDialog(result.getErrorCode());
+            resolvingError = true;
+        }
+    }
+
+    private void showErrorDialog(int errorCode) {
+        final ErrorDialogFragment dialogFragment;
+        final Bundle              args;
+
+        dialogFragment = new ErrorDialogFragment();
+        args           = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+    public void onDialogDismissed()
+    {
+        resolvingError = false;
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode,
+                                    final int resultCode,
+                                    final Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            resolvingError = false;
+
+            if(resultCode == RESULT_OK) {
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    public static class ErrorDialogFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final int errorCode;
+
+            errorCode = this.getArguments().getInt(DIALOG_ERROR);
+
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(),
+                    errorCode,
+                    REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            final Maps mainActivity;
+
+            mainActivity = (Maps) getActivity();
+            mainActivity.onDialogDismissed();
+        }
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
